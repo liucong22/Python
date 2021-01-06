57751d4b2deebde49a1c71c2c270baa3368bc099
liucong22 <lcong7912@163.com>
2021-01-07 00:38:00 +0800

    'fix:赌博游戏'

--- python/day03.py
+++ python/day03.py
@@ -28,7 +28,7 @@ print(res)
 
 """
     practice03
-    白钱白鸡：公鸡5元一只，母鸡3元一只，小鸡1元三只，用100块钱买一百只鸡，问公鸡、母鸡、小鸡各有多少只？
+    百钱百鸡：公鸡5元一只，母鸡3元一只，小鸡1元三只，用100块钱买一百只鸡，问公鸡、母鸡、小鸡各有多少只？
     author：liucong
     date：2021-01-06
 """
@@ -38,6 +38,53 @@ for x in range(20):
         z = 100 - x - y
         if x * 5 + y * 3 + z / 3 == 100:
             print("公鸡%d只，母鸡%d只，小鸡%d只" % (x, y, z))
+
+"""
+    practice04
+    CRAPS赌博游戏:
+    玩家第一次摇骰子如果摇出了7点或11点，玩家胜；
+    玩家第一次如果摇出2点、3点或12点，庄家胜；
+    其他点数玩家继续摇骰子，如果玩家摇出了7点，庄家胜；
+    如果玩家摇出了第一次摇的点数，玩家胜；
+    其他点数，玩家继续摇骰子，直到分出胜负。
+    author：liucong
+    date：2021-01-06
+"""
+from random import randint
+
+money = int(input("请输入资产："))
+while money > 0:
+    print("你的总资产为%d" % money)
+    needs_go_on = False
+    while True:
+        debt = int(input("请下注："))
+        if 0 < debt <= money:
+            break
+    # randint(a,b) [a, b]；range(a,b) [a, b)
+    first = randint(1, 6) + randint(1, 6)
+    print('玩家摇出了%d点' % first)
+    if first == 7 or first == 11:
+        print("玩家胜！")
+        money += debt
+    elif first == 2 or first == 3 or first == 12:
+        print("庄家胜！")
+        money -= debt
+    else:
+        needs_go_on = True
+    while needs_go_on:
+        needs_go_on = False
+        current = randint(1, 6) + randint(1, 6)
+        print('玩家摇出了%d点' % current)
+        if current == 7:
+            print("庄家胜！")
+            money -= debt 
+        elif current == first:
+            print("玩家胜！")
+            money += debt
+        else:
+            needs_go_on = True
+print("你破产了，游戏结束")
+
 
 
 
